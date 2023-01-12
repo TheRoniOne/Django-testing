4db3c312967d1b943627760c0b69c8aa97de86c5
TheRoniOne <ronidani99@hotmail.com>
2023-01-12 10:46:09 -0500

    Move templates to top level ad update settings accordingly

--- django_testing/settings.py
+++ django_testing/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "django_testing.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "django_testing" / "templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
